ff34d368b92a1a99aba833be41ba31f8f2a5dadc
Sabarish <121116004+th3n00bd3v@users.noreply.github.com>
2026-01-07 11:32:38 +0530

    Added code and answer.

--- week06/04_03_conditional_return.py
+++ week06/04_03_conditional_return.py
@@ -11,6 +11,15 @@ def greater_than_x(n, x):
        False otherwise
     """
 
+    if n>x:
+        return True
+    return False
+
 
 ### Extra credit
 # Is it required to use the else block? Can you write the above code with only the if statement but no else?
+
+'''
+Answer: It's not strictly necessary to use the 'else' block. 
+The same result can be achieved with just an 'if' statement followed by an implicit 'return'.
+'''
