cab72fbaaeeaf64d59e02b85fbc2ead37c53ff33
Sabarish <121116004+th3n00bd3v@users.noreply.github.com>
2026-01-07 11:37:53 +0530

    Added code and answer.

--- week06/04_04_exit_loop.py
+++ week06/04_04_exit_loop.py
@@ -20,3 +20,11 @@ def find_first_divisible(list_of_nums, n):
     ## check if the number is divisble by n with modulo
     ## if it is -- return the number immediately!
     ## This will exit the for-loop!!!
+
+    for num in list_of_nums:
+        if num % n == 0:
+            return num
+    return None
+    
+result = find_first_divisible([1, 2, 3, 4, 5], 7)
+print(result) # This will print None, as 7 is not divisible by any of the numbers in 'list_of_nums'
