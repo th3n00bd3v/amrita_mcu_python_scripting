8e85f037b9d47be390babd89c7d3afcbc433406f
Sabarish <121116004+th3n00bd3v@users.noreply.github.com>
2026-01-07 11:39:39 +0530

    Updated code and answer.

--- week06/05_01_no_arg.py
+++ week06/05_01_no_arg.py
@@ -1,5 +1,7 @@
 # write a function that takes no arguments
 
+# Define a global variable
+message = "Hello from the global scope!"
 
 def no_args():
     """
@@ -7,6 +9,16 @@ def no_args():
     ideas: use a global
     Idea: just print something out
     """
+    global message  # Access the global variable
+    print(message)  # Print the global message
 
 
 # call your function down here
+
+    """
+    This function takes no arguments and prints a message.
+    It also uses a global variable to demonstrate interaction with the global scope.
+    """
+
+# Call the function
+no_args()
