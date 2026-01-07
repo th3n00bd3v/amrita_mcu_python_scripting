f207a98a66ce5a17201173b74f9d64c873530520
Sabarish <121116004+th3n00bd3v@users.noreply.github.com>
2026-01-07 11:11:16 +0530

    Added answer to the questions
    Updated comments and added explanations for variable scope in the add_to_me function.

--- week06/03_01_scope_w_int.py
+++ week06/03_01_scope_w_int.py
@@ -18,4 +18,42 @@ def add_to_me(num):
 z = add_to_me(10)
 print(z)
 
-print(x)  # what do you think will print out?
+print(x)  # what do you think will print out? Answer: This will print z as 110 and x as 100.
+
+# 2. a.
+
+
+def add_to_me(num, x):
+    y = num + x
+    print(f"x inside the function is: {x}")
+    return y
+
+z = add_to_me(10, x)
+print(z)
+
+print(x) 
+
+'''
+In this version, x is now explicitly passed as an argument when calling the function add_to_me(10, x). 
+Inside the function, x is treated as a local variable, and it prints the same value as passed to it (which is the global x).
+'''
+
+# b.
+
+x = 100
+
+def add_to_me(num):
+    x = 200  # New local variable
+    y = num + x
+    print(f"x inside the function is: {x}")
+    return y
+
+z = add_to_me(10)
+print(z)
+
+print(x)
+
+'''
+Here, a new x is created within the function and set to 200. This local variable shadows the global x within the function scope. 
+The output will be: 210 for z and 100 for x.
+'''
