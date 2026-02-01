867fc09d9c84aa633ddfafbaa1425da6f65c8694
th3n00bd3v <121116004+th3n00bd3v@users.noreply.github.com>
2026-02-01 14:27:52 +0530

    Week 14 completed

--- week14 - automation/03_02_pokemon.py
+++ week14 - automation/03_02_pokemon.py
@@ -9,3 +9,39 @@ BONUS: Using your script, create a folder and download the main 'front_default'
        Name the files appropriately using the name data from your response.
 """
 
+
+from pprint import pprint
+import requests
+import os
+
+def fetch_pokemon_data(pokemon_id):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/"
+    response = requests.get(url).json()
+    return {
+        "name": response["name"],
+        "height": response["height"],
+        "sprite_url": response["sprites"]["front_default"]
+    }
+
+def save_pokemon_info(pokemon_data, filename="pokemon_info.txt"):
+    with open(filename, "a") as file:
+        file.write(f"Name: {pokemon_data['name'].title()}\n")
+        file.write(f"Height: {pokemon_data['height']}\n")
+        file.write("\n")
+        
+def download_sprite(pokemon_data, folder="pokemon_sprites"):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
+    sprite_url = pokemon_data["sprite_url"]
+    if sprite_url:
+        response = requests.get(sprite_url)
+        with open(os.path.join(folder, f"{pokemon_data['name']}.png"), "wb") as file:
+            file.write(response.content)
+            
+for pokemon_id in range(1, 152):
+    pokemon_data = fetch_pokemon_data(pokemon_id)
+    save_pokemon_info(pokemon_data)
+    download_sprite(pokemon_data)
+    
+print(f"Pokémon information and sprites have been saved.Check the folder {pokemon_sprites} for images.")
--- week14 - automation/03_03_chuck_norris_poetry.py
+++ week14 - automation/03_03_chuck_norris_poetry.py
@@ -8,3 +8,34 @@ Using the Chuck Norris API in combination with the datamuse API
 * Synthesize the collected results into an avant-garde poem and post on the forum ;)
 """
 
+from pprint import pprint
+import requests
+
+def fetch_chuck_norris_joke():
+    url = "https://api.chucknorris.io/jokes/random"
+    response = requests.get(url).json()
+    return response["value"]
+  
+def fetch_rhyming_word(word):
+    url = f"https://api.datamuse.com/words?rel_rhy={word}"
+    response = requests.get(url).json()
+    if response:
+        return response[0]["word"]
+    return None
+  
+poem_lines = []
+for _ in range(5):
+    joke = fetch_chuck_norris_joke()
+    last_word = joke.split()[-1].strip('.,!?;"\'').lower()
+    rhyme_word = fetch_rhyming_word(last_word)
+    
+    if rhyme_word:
+        poem_lines.append(f"{joke} ... {rhyme_word}")
+    else:
+        poem_lines.append(joke)
+        
+print("Avant-Garde Chuck Norris Poem:\n")
+for line in poem_lines:
+    print(line)
+    
+print("\nEnjoy your avant-garde poem!")
